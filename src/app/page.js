593c535d0b802f1b1c94f330fe76ffc3b849e4ef
Shadid12 <Shadid12@users.noreply.github.com>
2024-05-10 08:33:32 -0400

    video-3

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,39 @@
+'use client'
 import styles from "./page.module.css";
+import { useState } from "react";
+import { Client, fql } from "fauna";
 
 export default function Home() {
+  const [roomName, setRoomName] = useState("");
+
+  const client = new Client({ secret: process.env.NEXT_PUBLIC_FAUNA_KEY });
+
+  const createNewRoom = async (e) => {
+    e.preventDefault();
+    try {
+      const newroom = await client.query(fql`
+        Room.create({
+          name: ${roomName},
+        })
+      `);
+      console.log(newroom);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <h1>Chat App</h1>
+        <form onSubmit={createNewRoom}>
+          <input 
+            type="text" 
+            placeholder="Room name" 
+            value={roomName} 
+            onChange={(e) => setRoomName(e.target.value)} 
+          />
+          <button type="submit">Create room</button>
+        </form>
       </div>
     </main>
   );
