85f94f648eb01d18e642fcd7d40c1a5ac982b0c0
Shadid12 <Shadid12@users.noreply.github.com>
2024-05-13 14:18:39 -0400

    video-4-showing-rooms-inrealtime

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,57 @@
 'use client'
 import styles from "./page.module.css";
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Client, fql } from "fauna";
 
 export default function Home() {
   const [roomName, setRoomName] = useState("");
+  const [existingRooms, setExistingRooms] = useState([]);
+
+  const streamRef = useRef(null);
 
   const client = new Client({ secret: process.env.NEXT_PUBLIC_FAUNA_KEY });
+
+  const fetchRooms = async () => {
+    try {
+      const response = await client.query(fql`
+        Room.all()
+      `);
+      setExistingRooms(response.data.data);
+      if(!streamRef.current) {
+        const stream = await client.stream(fql`Room.all().toStream()`);
+        streamRef.current = stream;
+        for await (const event of stream) {
+          console.log(event);
+          switch(event.type) {
+            case "add":
+            case "update":
+              setExistingRooms(prev => {
+                const existingRoom = prev.findIndex(room => 
+                  room.id === event?.data.id
+                );
+                if (existingRoom === -1) {
+                  return [...prev, event.data];
+                } else {
+                  return prev.map(room => {
+                    if (room.id === event?.data.id) {
+                      return event.data;
+                    }
+                    return room;
+                  });
+                }  
+              })
+          }
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!client) return;
+    fetchRooms();
+  }, [])
 
   const createNewRoom = async (e) => {
     e.preventDefault();
@@ -34,6 +79,14 @@ export default function Home() {
           />
           <button type="submit">Create room</button>
         </form>
+        <div>
+          <h2>Existing rooms</h2>
+          {existingRooms.map((room) => (
+            <div key={room?.id}>
+              <button>{room.name}</button>
+            </div>
+          ))}
+        </div>
       </div>
     </main>
   );
