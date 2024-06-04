dbc42d2f77991e339b49e0597c37bfa2061e964e
Shadid12 <Shadid12@users.noreply.github.com>
2024-06-03 20:31:42 -0400

    video 10 complete. refactor compoennts

--- src/app/page.js
+++ src/app/page.js
@@ -1,62 +1,29 @@
-'use client'
 import styles from "./page.module.css";
-import { useState, useEffect, useRef } from "react";
 import { Client, fql } from "fauna";
-import Link from "next/link";
+import RoomList from "./components/RoomList";
 
-export default function Home() {
-  const [roomName, setRoomName] = useState("");
-  const [existingRooms, setExistingRooms] = useState([]);
-
-  const streamRef = useRef(null);
+export default async function Home() {
 
   const client = new Client({ secret: process.env.NEXT_PUBLIC_FAUNA_KEY });
+  // server side rendering
+  const roomsResponse = await client.query(fql`Room.all()`);
 
-  const fetchRooms = async () => {
+  const rooms = roomsResponse.data?.data ? 
+    roomsResponse.data.data.map(room => ({
+      id: room.id,
+      name: room.name
+    })) : [];
+
+
+  
+
+  const createNewRoom = async (formData) => {
+    'use server'
+    const client = new Client({ 
+      secret: process.env.NEXT_PUBLIC_FAUNA_KEY 
+    });
     try {
-      const response = await client.query(fql`
-        Room.all()
-      `);
-      setExistingRooms(response.data.data);
-      if(!streamRef.current) {
-        const stream = await client.stream(fql`Room.all().toStream()`);
-        streamRef.current = stream;
-        for await (const event of stream) {
-          console.log(event);
-          switch(event.type) {
-            case "add":
-            case "update":
-              setExistingRooms(prev => {
-                const existingRoom = prev.findIndex(room => 
-                  room.id === event?.data.id
-                );
-                if (existingRoom === -1) {
-                  return [...prev, event.data];
-                } else {
-                  return prev.map(room => {
-                    if (room.id === event?.data.id) {
-                      return event.data;
-                    }
-                    return room;
-                  });
-                }  
-              })
-          }
-        }
-      }
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  useEffect(() => {
-    if (!client) return;
-    fetchRooms();
-  }, [])
-
-  const createNewRoom = async (e) => {
-    e.preventDefault();
-    try {
+      const roomName = formData.get('roomName');
       const newroom = await client.query(fql`
         Room.create({
           name: ${roomName},
@@ -71,23 +38,15 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
-        <form onSubmit={createNewRoom}>
+        <form action={createNewRoom}>
           <input 
             type="text" 
-            placeholder="Room name" 
-            value={roomName} 
-            onChange={(e) => setRoomName(e.target.value)} 
+            placeholder="Room name"
+            name="roomName" 
           />
           <button type="submit">Create room</button>
         </form>
-        <div>
-          <h2>Existing rooms</h2>
-          {existingRooms.map((room) => (
-            <div key={room?.id}>
-              <Link href={`/rooms/${room.id}`}>{room.name}</Link>
-            </div>
-          ))}
-        </div>
+        <RoomList rooms={rooms} />
       </div>
     </main>
   );
